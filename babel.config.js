542a4a040efe46076a66733a93a109e84c1ccbea
Yomance <17631424475@163.com>
2020-07-20 21:18:57 +0800

    添加vant样式配置

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
+  presets: ["@vue/cli-plugin-babel/preset"],
+  // plugins vant 样式插件
+  plugins: [
+    [
+      "import",
+      {
+        libraryName: "vant",
+        libraryDirectory: "es",
+        style: true,
+      },
+      "vant",
+    ],
+  ],
+};
